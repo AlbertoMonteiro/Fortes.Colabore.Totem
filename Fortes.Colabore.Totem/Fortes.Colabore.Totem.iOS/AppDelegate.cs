3f0a289bee467bd19464d31188adb6c2ddd328b2
Alberto Monteiro <alberto.monteiro@live.com>
2021-03-23 12:05:13 -0300

    Reading QR Code

--- Fortes.Colabore.Totem/Fortes.Colabore.Totem.iOS/AppDelegate.cs
+++ Fortes.Colabore.Totem/Fortes.Colabore.Totem.iOS/AppDelegate.cs
@@ -22,6 +22,7 @@ namespace Fortes.Colabore.Totem.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            ZXing.Net.Mobile.Forms.iOS.Platform.Init();
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
--- Fortes.Colabore.Totem/Fortes.Colabore.Totem/ViewModels/AboutViewModel.cs
+++ Fortes.Colabore.Totem/Fortes.Colabore.Totem/ViewModels/AboutViewModel.cs
@@ -1,18 +1,42 @@
-﻿using System;
+﻿using System.Threading.Tasks;
 using System.Windows.Input;
-using Xamarin.Essentials;
 using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
 
 namespace Fortes.Colabore.Totem.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        private string barCodeValue = "QR não lido";
+
         public AboutViewModel()
         {
             Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            OpenWebCommand = new Command(async () => await OpenQRCodeScanAsync());
+        }
+
+        private async Task OpenQRCodeScanAsync()
+        {
+            var scanPage = new ZXingScannerPage();
+            scanPage.OnScanResult += (result) =>
+            {
+                scanPage.IsScanning = false;
+
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    BarCodeValue = result.Text;
+                    await Shell.Current.Navigation.PopAsync();
+                });
+            };
+
+            await Shell.Current.Navigation.PushAsync(scanPage);
         }
 
         public ICommand OpenWebCommand { get; }
+        public string BarCodeValue 
+        { 
+            get => barCodeValue; 
+            set => SetProperty(ref barCodeValue, value); 
+        }
     }
 }
